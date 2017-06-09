623c27a13dfc4a1360ff9853caec356ce74f36da
Johan Wilhelmsson <rave@Raves-MacBook-Pro-5.local>
2017-06-09 05:18:14 +0100

    new video jquery

--- js/main.js
+++ js/main.js
@@ -44,6 +44,14 @@ console.log(changeBackground10);
 //   });
 // });
 
+$(document).ready(function(){
+    $("a").click(function(e) {
+        e.preventDefault();
+
+        $("#iconVideos").attr("src", $(this).attr("href"));
+    })
+});
+
 
 $(function showText(){
     $('#swichVid50').click(function(){
