4d8c7d01384cd12b116e01ffcec211a77484a677
Johan Wilhelmsson <rave@Raves-MacBook-Pro-5.local>
2017-06-09 06:19:35 +0100

    try again once more

--- js/main.js
+++ js/main.js
@@ -52,9 +52,9 @@ console.log(changeBackground10);
 //     })
 // });
 
-$(function video50() {
+function video50() {
   $("#iconVideos source").attr("src", "https://www.youtube.com/watch?v=EQPcwqPL34I&feature=youtu.be");
-})
+}
 
 
 $(function showText(){
