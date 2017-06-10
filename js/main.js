4289285ea44246a06a5cecf55facb6adf682cbb1
Johan Wilhelmsson <rave@raves-mbp-5.client.rave.ac.uk>
2017-06-10 16:42:39 +0100

    added other text box text

--- js/main.js
+++ js/main.js
@@ -40,6 +40,7 @@ $(function showText(){
     $('#swichVid50').click(function(){
       $('#textBox').html('<p> Hound Dog has been recorded more than 250 times! Elvis Presley version ranked No.19 on in the Greatest Songs of All Time. It’s is also one of the best-selling singles of all time. Presleys version, which sold about 10 million copies globally, was his best-selling song and an emblem of the rock n roll revolution.</p>');
       $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/EQPcwqPL34I?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1&loop=1" width="235px" height="180px" frameborder="0" ></iframe>');
+      $('#otherSongs').html('<p>Little Richard - "Tutti Frutti"</p><br /><p> Chuck Berry - "Johnny B. Goode"</p><br /><p> Jerry Lee Lewis - "Great Balls of Fire"</p>')
     });
   });
 
@@ -48,6 +49,7 @@ $(function showText(){
       $('#swichVid60').click(function(){
         $('#textBox').html('<p>Here Comes the Sun was released by the Beatles in 1969 on album Abbey Road. The song was written at the country house of Eric Clapton, The lyrics reflect the composers relief at both the arrival of spring and the temporary respite he was experiencing from the bands business affairs.</p>');
         $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/fPiw-pHUJ5w?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1" width="235px" height="180px" frameborder="0" ></iframe>');
+        $('#otherSongs').html('<p></p>')
       });
     });
 
@@ -55,8 +57,7 @@ $(function showText(){
       $('#swichVid70').click(function(){
         $('#textBox').html('<p>Stayin Alive was released on the 4 February 1978, it remained at the top of the uk charts for a whopping 13 weeks. In the process, it became one of the bands most recognisable tunes, in part because of its place at the beginning of Saturday Night Fever. </p>');
         $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/AwWk2UUdC-o?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1" width="235px" height="180px" frameborder="0"> </iframe>');
-        // $('#videoBox').html('<img src="http://i.imgur.com/YwDxaeq.png" />');
-
+        $('#otherSongs').html('<p></p>')
       });
     });
 
@@ -64,7 +65,7 @@ $(function showText(){
     $('#swichVid80').click(function(){
       $('#textBox').html('<p>Simply the best was originally recorded by Bonnie Tyler in 1988. The song was later covered by Tina Turner, and released as a highly successful single the year after. </p>');
       $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/6CsO1-WVaIM?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1" width="235px" height="180px" frameborder="0" ></iframe>');
-      // $('#videoBox').html('<img src="http://i.imgur.com/YwDxaeq.png" />');
+      $('#otherSongs').html('<p></p>')
     });
   });
 
@@ -72,15 +73,15 @@ $(function showText(){
     $('#swichVid90').click(function(){
       $('#textBox').html('<p> Juicy was released by american hip hop artist The Notorious B.I.G.in 1994, considered by the rolling stone as the one of the greatest hip hop songs it sold over 607,000 copies.</p>');
       $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/o4JB5l0bVIA?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1" width="235px" height="180px" frameborder="0" ></iframe>');
-      // $('#videoBox').html('<img src="http://i.imgur.com/YwDxaeq.png" />');
+      $('#otherSongs').html('<p></p>')
+      });
     });
-  });
 
   $(function moreText(){
     $('#swichVid00').click(function(){
       $('#textBox').html('<p>Toxic was released by american singer Britney Spears in 2004, attaining worldwide success and reaching the top-five in 15 countries.</p>');
       $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/nnqOq4nbsaU?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1" width="235px" height="180px" frameborder="0" ></iframe><');
-      // $('#videoBox').html('<img src="http://i.imgur.com/YwDxaeq.png" />');
+      $('#otherSongs').html('<p></p>')
     });
   });
 
@@ -88,6 +89,6 @@ $(function showText(){
     $('#swichVid10').click(function(){
       $('#textBox').html('<p>Gangnam Style was released by korean musician Psy and has been youtubes most watched video since 2012 , being viewed over 2.8 billion times.</p>');
       $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/WFao-AuupMw?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1" width="235px" height="180px" frameborder="0" ></iframe>');
-      // $('#videoBox').html('<img src="http://i.imgur.com/YwDxaeq.png" />');
-    });
+      $('#otherSongs').html('<p></p>')
+      });
   });
