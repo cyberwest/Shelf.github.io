8a20d0c1654ef7af9669ae09209ca8a10847d0b6
Johan Wilhelmsson <rave@raves-mbp-5.client.rave.ac.uk>
2017-06-10 16:05:45 +0100

    color

--- js/main.js
+++ js/main.js
@@ -46,14 +46,14 @@ $(function showText(){
 
   $(function moreText(){
       $('#swichVid60').click(function(){
-        $('#textBox').html('Here Comes the Sun" was released by the Beatles in 1969 on album Abbey Road. The song was written at the country house of Eric Clapton, The lyrics reflect the composers relief at both the arrival of spring and the temporary respite he was experiencing from the bands business affairs.');
+        $('#textBox').html('<p>Here Comes the Sun" was released by the Beatles in 1969 on album Abbey Road. The song was written at the country house of Eric Clapton, The lyrics reflect the composers relief at both the arrival of spring and the temporary respite he was experiencing from the bands business affairs.</p>');
         $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/fPiw-pHUJ5w?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1" width="235px" height="180px" frameborder="0" ></iframe>');
       });
     });
 
   $(function moreText(){
       $('#swichVid70').click(function(){
-        $('#textBox').html('Stayin Alive was released on the 4 February 1978, it remained at the top of the uk charts for a whopping 13 weeks. In the process, it became one of the bands most recognisable tunes, in part because of its place at the beginning of Saturday Night Fever. ');
+        $('#textBox').html('<p>Stayin Alive was released on the 4 February 1978, it remained at the top of the uk charts for a whopping 13 weeks. In the process, it became one of the bands most recognisable tunes, in part because of its place at the beginning of Saturday Night Fever. </p>');
         $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/AwWk2UUdC-o?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1" width="235px" height="180px" frameborder="0"> </iframe>');
         // $('#videoBox').html('<img src="http://i.imgur.com/YwDxaeq.png" />');
 
@@ -62,7 +62,7 @@ $(function showText(){
 
   $(function moreText(){
     $('#swichVid80').click(function(){
-      $('#textBox').html('Simply the best was originally recorded by Bonnie Tyler in 1988. The song was later covered by Tina Turner, and released as a highly successful single the year after. ');
+      $('#textBox').html('<p>Simply the best was originally recorded by Bonnie Tyler in 1988. The song was later covered by Tina Turner, and released as a highly successful single the year after. </p>');
       $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/6CsO1-WVaIM?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1" width="235px" height="180px" frameborder="0" ></iframe>');
       // $('#videoBox').html('<img src="http://i.imgur.com/YwDxaeq.png" />');
     });
@@ -70,7 +70,7 @@ $(function showText(){
 
   $(function moreText(){
     $('#swichVid90').click(function(){
-      $('#textBox').html(' Juicy was released by american hip hop artist The Notorious B.I.G.in 1994, considered by the rolling stone as the one of the greatest hip hop songs it sold over 607,000 copies.');
+      $('#textBox').html('<p> Juicy was released by american hip hop artist The Notorious B.I.G.in 1994, considered by the rolling stone as the one of the greatest hip hop songs it sold over 607,000 copies.</p>');
       $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/o4JB5l0bVIA?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1" width="235px" height="180px" frameborder="0" ></iframe>');
       // $('#videoBox').html('<img src="http://i.imgur.com/YwDxaeq.png" />');
     });
@@ -78,7 +78,7 @@ $(function showText(){
 
   $(function moreText(){
     $('#swichVid00').click(function(){
-      $('#textBox').html('Toxic was released by american singer Britney Spears in 2004, attaining worldwide success and reaching the top-five in 15 countries.');
+      $('#textBox').html('<p>Toxic was released by american singer Britney Spears in 2004, attaining worldwide success and reaching the top-five in 15 countries.</p>');
       $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/nnqOq4nbsaU?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1" width="235px" height="180px" frameborder="0" ></iframe><');
       // $('#videoBox').html('<img src="http://i.imgur.com/YwDxaeq.png" />');
     });
@@ -86,7 +86,7 @@ $(function showText(){
 
   $(function moreText(){
     $('#swichVid10').click(function(){
-      $('#textBox').html('“Gangnam Style” was released by korean musician Psy and has been youtubes most watched video since 2012 , being viewed over 2.8 billion times.');
+      $('#textBox').html('“<p>Gangnam Style” was released by korean musician Psy and has been youtubes most watched video since 2012 , being viewed over 2.8 billion times.</p>');
       $('#videoContainer').html('<iframe src="https://www.youtube.com/embed/WFao-AuupMw?rel=0&amp;controls=0&amp;showinfo=0?ecver=2&amp;autoplay=1" width="235px" height="180px" frameborder="0" ></iframe>');
       // $('#videoBox').html('<img src="http://i.imgur.com/YwDxaeq.png" />');
     });
